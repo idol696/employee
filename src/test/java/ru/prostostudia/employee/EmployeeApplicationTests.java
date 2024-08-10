9e5d998cb27b9b0d80e8b6adafed34d32ff16afb
Roman Kuzmin <ghelatin@mail.ru>
2024-08-10 15:37:45 +0300

    init commit

---
+++ src/test/java/ru/prostostudia/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
